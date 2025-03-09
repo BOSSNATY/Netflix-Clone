e47c7490ba47d546af2af229cb84c5b69b183df7
BOSSNATY <natnaelweldemariam29@gmail.com>
2025-03-09 16:03:42 +0300

    request modified

--- Netflix/src/utils/requests.jsx
+++ Netflix/src/utils/requests.jsx
@@ -2,6 +2,7 @@ API_KEY = process.env.REACT.APP.API.KEY;
 
 const request = {
   fetchTrending: `/trending/movie/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=en-US&page=1`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   fetchAction: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedy: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
