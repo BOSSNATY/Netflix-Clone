bcdc33c3e100b4651ffe96fa7d96433d76b6f794
BOSSNATY <natnaelweldemariam29@gmail.com>
2025-03-09 15:34:50 +0300

    axios and request

---
+++ Netflix/src/utils/axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const instance = axios.get({
+  baseURL: "https://api.themoviedb.org/3/",
+});
---
+++ Netflix/src/utils/requests.js
@@ -0,0 +1,13 @@
+API_KEY = process.env.REACT.APP.API.KEY;
+
+const request = {
+  fetchTrending: `/trending/movie/week?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchAction: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedy: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchHorror: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomance: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchScientificFiction: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchTVShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
